56422dae49b4436da8d95b6898474c49e8e0eaf2
Bram De Moor <bram.demoor@gmail.com>
2013-05-20 15:11:21 +0200

    Powerup pickup

--- PlatformerPOC/Domain/Powerup/Coin.cs
+++ PlatformerPOC/Domain/Powerup/Coin.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using PlatformerPOC.Helpers;
 
 namespace PlatformerPOC.Domain.Powerup
 {
@@ -7,11 +8,34 @@ namespace PlatformerPOC.Domain.Powerup
         public Coin(PlatformGame game, Vector2 pos) : base(game)
         {
             Position = pos;
+
+            BoundingBox = new CustomBoundingBox();
+            UpdateBoundingBox();
         }
 
         public override void Draw()
         {
             game.SpriteBatch.Draw(game.ResourcePreloader.Coin, PositionRelativeToView, Color.White);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            UpdateBoundingBox();
+
+            foreach (var player in game.PlayerManager.AlivePlayers)
+            {
+                if (CollisionHelper.RectangleCollision(BoundingBox.FullRectangle, player.BoundingBox.FullRectangle))
+                {
+                    DestroyEntity();
+                }
+            }
+        }
+
+        private void UpdateBoundingBox()
+        {
+            BoundingBox.SetFullRectangle(Position, game.ResourcePreloader.Coin.Bounds, Velocity);
         }
     }
 }
